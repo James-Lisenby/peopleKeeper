f5422871132518dd3b7f18618a6bed9847a462fc
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-09-29 10:49:21 -0700

    created connection.js

---
+++ connection.js
@@ -0,0 +1,14 @@
+
+const mysql = require('mysql');
+// Connection for SQL database
+module.exports = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+  
+    // Your username
+    user: 'root',
+  
+    // Your password
+    password: 'Beavertrapper69.',
+    database: 'castleKeeper_db'
+  });
